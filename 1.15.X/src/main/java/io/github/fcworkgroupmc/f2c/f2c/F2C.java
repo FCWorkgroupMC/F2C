ab0ea36ed595652e2f324f9e2ae65ed073f5e033
XiaoPangxie732 <3330116380@qq.com>
2020-06-07 12:17:33 +0800

    Develop commit

---
+++ 1.15.X/src/main/java/io/github/fcworkgroupmc/f2c/f2c/F2C.java
@@ -0,0 +1,16 @@
+package io.github.fcworkgroupmc.f2c.f2c;
+
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("f2c")
+public class F2C {
+	public F2C() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(EventPriority.HIGHEST, this::a);
+	}
+	public void a(FMLCommonSetupEvent event) {
+
+	}
+}
